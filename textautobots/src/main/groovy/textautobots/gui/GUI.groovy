f49328dff673e6d5353e75986ba3802664a1019e
Stelios Omirou <selemis@gmail.com>
2022-11-09 01:34:57 +0200

    add text areas in gui

--- textautobots/src/main/groovy/textautobots/gui/GUI.groovy
+++ textautobots/src/main/groovy/textautobots/gui/GUI.groovy
@@ -1,8 +1,13 @@
 package textautobots.gui
 
+import javax.swing.BoxLayout
 import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JTextArea
 import java.awt.BorderLayout
 import java.awt.HeadlessException
+import java.awt.Panel
 
 class GUI extends JFrame{
 
@@ -10,10 +15,40 @@ class GUI extends JFrame{
         setDefaultCloseOperation(EXIT_ON_CLOSE)
         setTitle("Text Autobots")
         getContentPane().setLayout(new BorderLayout())
-//        getContentPane().add(mainPanel(), BorderLayout.CENTER)
+        getContentPane().add(textAreasPanel(), BorderLayout.CENTER)
 //        getContentPane().add(createFilterListPanel(), BorderLayout.EAST)
         setSize(800, 600)
         setVisible(true)
     }
+
+    private JPanel textAreasPanel() {
+        JPanel panel = new JPanel()
+        BoxLayout boxLayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+        panel.setLayout(boxLayout)
+        panel.add(sourceTextAreaPanel())
+        panel.add(resultsTextAreaPanel())
+        panel
+    }
+
+    private JPanel sourceTextAreaPanel() {
+        JPanel panel = new JPanel()
+        panel.setLayout(new BorderLayout())
+        JLabel label = new JLabel("Source Text")
+        JTextArea textArea = new JTextArea(5, 80)
+        panel.add(label, BorderLayout.NORTH)
+        panel.add(textArea, BorderLayout.CENTER)
+        panel
+    }
+
+    private JPanel resultsTextAreaPanel() {
+        JPanel panel = new JPanel()
+        panel.setLayout(new BorderLayout())
+        JLabel label = new JLabel("Results")
+        JTextArea textArea = new JTextArea(5, 80)
+        panel.add(label, BorderLayout.NORTH)
+        panel.add(textArea, BorderLayout.CENTER)
+        panel
+    }
+
 
 }
