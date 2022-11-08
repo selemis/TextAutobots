2631a5b45b92a66bca0d53f61b9e1c4764b874a8
Stelios Omirou <selemis@gmail.com>
2022-11-09 01:40:40 +0200

    add a filtered list in gui

--- textautobots/src/main/groovy/textautobots/gui/GUI.groovy
+++ textautobots/src/main/groovy/textautobots/gui/GUI.groovy
@@ -1,22 +1,21 @@
 package textautobots.gui
 
-import javax.swing.BoxLayout
-import javax.swing.JFrame
-import javax.swing.JLabel
-import javax.swing.JPanel
-import javax.swing.JTextArea
-import java.awt.BorderLayout
-import java.awt.HeadlessException
-import java.awt.Panel
+import javax.swing.*
+import java.awt.*
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 
-class GUI extends JFrame{
+import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS
+
+class GUI extends JFrame {
 
     GUI() throws HeadlessException {
         setDefaultCloseOperation(EXIT_ON_CLOSE)
         setTitle("Text Autobots")
         getContentPane().setLayout(new BorderLayout())
         getContentPane().add(textAreasPanel(), BorderLayout.CENTER)
-//        getContentPane().add(createFilterListPanel(), BorderLayout.EAST)
+        getContentPane().add(createFilterListPanel(), BorderLayout.EAST)
         setSize(800, 600)
         setVisible(true)
     }
@@ -48,6 +47,44 @@ class GUI extends JFrame{
         panel.add(label, BorderLayout.NORTH)
         panel.add(textArea, BorderLayout.CENTER)
         panel
+    }
+
+    private JPanel createFilterListPanel() {
+        JPanel panel = new JPanel()
+        panel.setLayout(new BorderLayout())
+        FilteredJList filteredList = createFilteredJList()
+        filteredList.addMouseListener(new MouseAdapter() {
+            @Override
+            void mouseClicked(MouseEvent e) {
+                JList<String> source = (JList<String>) e.getSource()
+                if (e.getClickCount() == 2) {
+                    int index = source.locationToIndex(e.getPoint());
+                    ListModel<String> model = source.getModel()
+                    String elementAt = model.getElementAt(index)
+                    System.out.println(elementAt)
+//                    doSomething(index)
+                }
+
+            }
+        })
+        JScrollPane pane = new JScrollPane(filteredList, VERTICAL_SCROLLBAR_ALWAYS, HORIZONTAL_SCROLLBAR_NEVER);
+        panel.add(pane, BorderLayout.CENTER)
+        panel.add(filteredList.getFilterField(), BorderLayout.NORTH);
+        return panel
+    }
+
+    private FilteredJList createFilteredJList() {
+        def listItems = [
+            "Chris" , "Joshua" , "Daniel" , "Michael"
+        ]
+
+        FilteredJList list = new FilteredJList();
+        listItems.each {
+            list.addItem(it)
+        }
+
+
+        return list;
     }
 
 
